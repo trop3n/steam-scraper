81bccc02ae53ca5779384c5d1e38960d22e429b1
trop3n <jasondkimm@proton.me>
2025-02-18 22:02:44 -0600

    cleared README

--- steamscrape.py
+++ steamscrape.py
@@ -3,3 +3,5 @@ import lxml.html
 
 html = requests.get('https://store.steampowered.com/explore/new')
 doc = lxml.html.fromstring(html.content)
+
+new_releases = doc.xpath('//div[@id="tab_newreleases_content"]')[0]
