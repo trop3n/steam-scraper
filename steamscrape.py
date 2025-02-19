9f2be45d0a73425d134c9129fca270b7a67d27d1
trop3n <jasondkimm@proton.me>
2025-02-19 13:50:27 -0600

    completed script, ready to test

--- steamscrape.py
+++ steamscrape.py
@@ -12,3 +12,24 @@ tags = []
 for tag in new_releases.xpath('.//div[@class="tab_top_item_tags"]'):
     tags.apppend(tag.text_content())
 
+tags = [tags.split(', ') for tag in tags]
+platforms_div = new_releases.xpath('.//div[@class="tab_item_details"]')
+total_platforms = []
+
+for game in platforms_div:
+    temp = game.xpath('.//span[contains(@class, "platform_img")]')
+    platforms = [t.get('class').split(' ')[-1] for t in temp]
+    if 'hmd_separator' in platforms:
+        platforms.remove('hmd_separator')
+    total_platforms.append(platforms)
+
+output = []
+for info in zip(titles, prices, tags, total_platforms):
+    resp = {}
+    resp['title'] = info[0]
+    resp['price'] = info[1]
+    resp['tags'] = info[2]
+    resp['platforms'] = info[3]
+    output.append(resp)
+
+print(output)
