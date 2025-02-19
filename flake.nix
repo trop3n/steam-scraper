5027ad5f80cf9fb41b9369c8d98de05fb54e671f
trop3n <jasondkimm@proton.me>
2025-02-18 20:59:24 -0600

    first commit

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Poetry python application";
+
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    poetry2nix,
+    ...
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          poetry2nix.overlays.default
+        ];
+      };
+
+      python = pkgs.python312;
+      projectDir = ./.;
+      overrides = pkgs.poetry2nix.overrides.withDefaults (final: prev: {
+        # Python dependency overrides go here
+      });
+    in {
+      packages.default = pkgs.poetry2nix.mkPoetryApplication {
+        inherit python projectDir overrides;
+        # Non-Python runtime dependencies go here
+        propogatedBuildInputs = [];
+      };
+
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          (pkgs.poetry2nix.mkPoetryEnv {
+            inherit python projectDir overrides;
+          })
+          pkgs.poetry
+        ];
+      };
+    });
+}
